f54d16dcabe9fe8b76ba2d1eabf709d2b5a9d814
NicholasPiano <nicholas.d.piano@gmail.com>
2014-05-22 17:21:12 +0100

    adding audio testing and making models.

--- transcription/models.py
+++ transcription/models.py
@@ -1,42 +1,60 @@
+#django
 from django.db import models
 from django.core.files import File
+
+#local
 from archive.models import Archive
+
+#util
+import wave
+import numpy
 
 
 #every time a set of transcriptions is requested, it is sent as a Job object.
 class Job(models.Model):
     #properties
-    user =
+    user = #user that requested the job
 
 
     #initialiser
     #instance methods
 
 class Transcription(models.Model):
-    #general properties
+    #properties
     utterance = models.TextField(max_length=200)
-    word_list = []
-    archive = models.ForeignKey(Archive)
+    word_list = [] #used to sort transcription objects
+    archive = models.ForeignKey(Archive) #one-to-many from archive-side (archive.transcription_set)
     user_dictionary = {} #list of users that have requested this transcription along information about their activity
+    audiofile = models.OneToOneField(AudioFile, primary_key=True)
 
     #initialiser
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        #
 
     #instance methods
-    #1. get waveform
-    def get_waveform(self):
 
 
 #holds audio file for transcription object
 class AudioFile(File):
 
     #properties
+    audiofile = #actual sound file in memory
     waveform = #holds waveform to be displayed to the user
 
     #initialiser
+    def __init__(self, ):
+
+
     #instance methods
+
 
     #holds JSON data about the shape of the waveform of the audio track
     class WaveForm(object):
+        #properties
+        waypoint_dictionary = {} #for each point on the x-axis
 
+        #initialiser
+        def __init__(self, audiofile):
+
+
+        #instance methods
