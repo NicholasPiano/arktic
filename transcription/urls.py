d11204ad8ffeca01b0cc8b7e5bcfbc8e44374964
NicholasPiano <nicholas.d.piano@gmail.com>
2014-06-02 23:59:03 +0100

    MVP sort of done.
    I can now make the transcription url increment the global variable
    which is the number of transcriptions and display the next one. This is
    not what I want, but it shows what can be done.

--- transcription/urls.py
+++ transcription/urls.py
@@ -2,8 +2,9 @@
 from django.conf.urls import patterns, include, url
 
 #local
-from transcription.views import index
+from transcription.views import index, next
 
 urlpatterns = patterns('',
-    url(r'', index, name='index'),
+    url(r'^$', index, name='index'),
+    url(r'^next/$', next, name='next')
 )
--- transcription/views.py
+++ transcription/views.py
@@ -2,7 +2,7 @@
 
 #django
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 #local
 from transcription.models import Transcription
@@ -14,4 +14,10 @@ def index(request):
     t = Transcription.objects.all()[transcription_index]
     return render(request, 'transcription/main_transcription.html', {
         'transcription':t ,
+        'count':transcription_index,
     })
+
+def next(request):
+    global transcription_index
+    transcription_index += 1
+    return HttpResponseRedirect('/transcription/')
