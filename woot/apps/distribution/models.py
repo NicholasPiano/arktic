568ed31d7cbf45f4fc5bc8efa29a8c25fcf24980
NicholasPiano <nicholas.d.piano@gmail.com>
2014-09-19 21:27:32 +0100

    Trying project save thing. Might fail.

--- woot/apps/distribution/models.py
+++ woot/apps/distribution/models.py
@@ -53,8 +53,9 @@ class Client(models.Model):
 
     def update(self):
         for project in self.projects.filter(is_active=False):
-            project.export()
-            project.save()
+            if not self.completed_projects.get(name=project.name+'_completed'):
+                project.export()
+                project.save()
 
 class Project(models.Model):
     #connections
