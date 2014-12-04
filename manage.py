9d438d4f6963a11bf28c905882c9f09e484a4ea3
NicholasPiano <nicholas.d.piano@gmail.com>
2014-12-04 17:26:06 +0000

    notes

--- manage.py
+++ manage.py
@@ -10,3 +10,6 @@ if __name__ == "__main__":
     execute_from_command_line(sys.argv)
 
 # from apps.transcription.models import Transcription, Revision;from apps.distribution.models import Action, Job, Project, CompletedProject;from apps.users.models import User
+
+# from apps.transcription.models import Transcription, Revision;from apps.distribution.models import Action, Job, Project, CompletedProject;from apps.users.models import User; p = Project.objects.get(pk=2); R = Revision.objects.all(); T = Transcription.objects.filter(project=p); A = Action.objects.all(); np = User.objects.get(pk=1); sj = User.objects.get(pk=2); gp = User.objects.get(pk=4); dd = User.objects.get(pk=5);
+
