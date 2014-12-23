c23ec88daecdc556dcb1ca17c67175a51d08b04a
NicholasPiano <nicholas.d.piano@gmail.com>
2014-12-23 17:43:03 +0000

    Switching to version 0.5
    Updates in issues.

--- woot/apps/distribution/models.py
+++ woot/apps/distribution/models.py
@@ -22,180 +22,180 @@ COMPLETED_PROJECT_ROOT = os.path.join(MEDIA_ROOT, 'completed')
 
 #classes
 class Client(models.Model):
-    #connections
-    #sub: projects, jobs, archives, relfiles, transcriptions, autocomplete words
+  #connections
+  #sub: projects, jobs, archives, relfiles, transcriptions, autocomplete words
 
-    #properties
-    name = models.CharField(max_length=255)
+  #properties
+  name = models.CharField(max_length=255)
 
-    def __unicode__(self):
-        return self.name
+  def __unicode__(self):
+    return self.name
 
-    #custom methods
-    def create_autocomplete_words(self):
-        #get list of current words
-        current_word_list = []
-        for word in self.words.all():
-            if word.content not in current_word_list:
-                current_word_list.append(word.content)
-        #get all unique words and phrases from transcriptions
-        new_word_list = []
-        for transcription in self.transcriptions.all():
-            if transcription.utterance not in current_word_list and transcription.utterance not in new_word_list:
-                new_word_list.append(transcription.utterance)
-                for word in transcription.utterance.split():
-                    if word not in current_word_list and word not in new_word_list:
-                        new_word_list.append(word)
+  #custom methods
+  def create_autocomplete_words(self):
+    #get list of current words
+    current_word_list = []
+    for word in self.words.all():
+      if word.content not in current_word_list:
+        current_word_list.append(word.content)
+    #get all unique words and phrases from transcriptions
+    new_word_list = []
+    for transcription in self.transcriptions.all():
+      if transcription.utterance not in current_word_list and transcription.utterance not in new_word_list:
+        new_word_list.append(transcription.utterance)
+        for word in transcription.utterance.split():
+          if word not in current_word_list and word not in new_word_list:
+            new_word_list.append(word)
 
-        #add one AutocompleteWord for each one.
-        for word in new_word_list:
-            self.words.create(content=word)
+    #add one AutocompleteWord for each one.
+    for word in new_word_list:
+      self.words.create(content=word)
 
-    def update(self):
-        for project in self.projects.filter(is_active=False):
-            if self.completed_projects.filter(name=project.name+'_completed') == []:
-                project.export()
-                project.save()
+  def update(self):
+    for project in self.projects.filter(is_active=False):
+        if self.completed_projects.filter(name=project.name+'_completed') == []:
+          project.export()
+          project.save()
 
 class Project(models.Model):
-    #connections
-    client = models.ForeignKey(Client, related_name='projects')
-    #sub: jobs, archives, relfiles, transcriptions
+  #connections
+  client = models.ForeignKey(Client, related_name='projects')
+  #sub: jobs, archives, relfiles, transcriptions
 
-    #properties
-    name = models.CharField(max_length=255)
-    date_created = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
+  #properties
+  name = models.CharField(max_length=255)
+  date_created = models.DateTimeField(auto_now_add=True)
+  is_active = models.BooleanField(default=True)
 
-    def __unicode__(self):
-        return str(self.client) + ' ' + self.name
+  def __unicode__(self):
+    return str(self.client) + ' ' + self.name
 
-    #custom methods
-    def update(self):
-        if self.transcriptions.filter(is_active=True).count()==0:
-            if self.is_active:
-                self.is_active = False
+  #custom methods
+  def update(self):
+    if self.transcriptions.filter(is_active=True).count()==0:
+      if self.is_active:
+        self.is_active = False
 
-    def export(self):
-        #create directory with name of project
-        os.makedirs(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed'))
+  def export(self):
+    #create directory with name of project
+    os.makedirs(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed'))
 
-        print('relfiles...')
-        for relfile in self.relfiles.all():
-            #open relfile and get contents
-            lines = relfile.file.file.readlines()
-            #open new file in completed directory
-            #new filename with '.out.csv'
-            with open(os.path.join(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed'), os.path.splitext(relfile.name)[0]+'.out.csv'), 'w+') as complete_relfile:
-                new_lines = []
-                for line_number, line in enumerate(lines):
-                    #find transcription by specifying line_number and grammar name
-                    tokens = line.split('|')
-#                     audio_file_name = os.path.basename(tokens[0]).rstrip()
-#                     grammar = os.path.splitext(os.path.basename(tokens[1]))[0]
+    print('relfiles...')
+    for relfile in self.relfiles.all():
+      #open relfile and get contents
+      lines = relfile.file.file.readlines()
+      #open new file in completed directory
+      #new filename with '.out.csv'
+      with open(os.path.join(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed'), os.path.splitext(relfile.name)[0]+'.out.csv'), 'w+') as complete_relfile:
+        new_lines = []
+        for line_number, line in enumerate(lines):
+          #find transcription by specifying line_number and grammar name
+          tokens = line.split('|')
+#           audio_file_name = os.path.basename(tokens[0]).rstrip()
+#           grammar = os.path.splitext(os.path.basename(tokens[1]))[0]
 
-                    ### NEEDS FIXING
+          ### NEEDS FIXING
 
-#                     transcriptions = self.transcriptions.filter(line_number=line_number, relfile=relfile) #set of transcriptions until grammar thing is fixed
-                    transcription = self.transcriptions.get(line_number=line_number, relfile=relfile) #set of transcriptions until grammar thing is fixed
-#                     for t in transcriptions:
-#                       if t.audio_file.name == audio_file_name:
-#                         transcription = t
-#                         break
+#           transcriptions = self.transcriptions.filter(line_number=line_number, relfile=relfile) #set of transcriptions until grammar thing is fixed
+          transcription = self.transcriptions.get(line_number=line_number, relfile=relfile) #set of transcriptions until grammar thing is fixed
+#           for t in transcriptions:
+#             if t.audio_file.name == audio_file_name:
+#               transcription = t
+#               break
 
-                    ###
+          ###
 
-                    latest_revision = transcription.revisions.latest() #must exist
-                    print(str(relfile) + ' %d'%line_number + ' ' + transcription.utterance + ' > ' + latest_revision.utterance)
-                    tokens[3] = latest_revision.utterance
-                    new_line = '|'.join(tokens)
-                    new_lines.append(new_line)
-                for new_line in new_lines:
-                    complete_relfile.write(new_line.encode('utf-8'))
-                self.completed_relfiles.create(client=self.client, archive=relfile.archive, relfile=relfile, file=File(complete_relfile), name=relfile.name)
+          latest_revision = transcription.revisions.latest() #must exist
+          print(str(relfile) + ' %d'%line_number + ' ' + transcription.utterance + ' > ' + latest_revision.utterance)
+          tokens[3] = latest_revision.utterance
+          new_line = '|'.join(tokens)
+          new_lines.append(new_line)
+        for new_line in new_lines:
+          complete_relfile.write(new_line.encode('utf-8'))
+          self.completed_relfiles.create(client=self.client, archive=relfile.archive, relfile=relfile, file=File(complete_relfile), name=relfile.name)
 
-        #zip directory
-        print('zip...')
-        zip_file = zp.ZipFile(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed.zip'), 'w', zp.ZIP_DEFLATED)
-        for f in self.completed_relfiles.all():
-            file_path = f.file.file.name
-            zip_file.write(file_path, os.path.relpath(file_path, COMPLETED_PROJECT_ROOT))
+    #zip directory
+    print('zip...')
+    zip_file = zp.ZipFile(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed.zip'), 'w', zp.ZIP_DEFLATED)
+    for f in self.completed_relfiles.all():
+      file_path = f.file.file.name
+      zip_file.write(file_path, os.path.relpath(file_path, COMPLETED_PROJECT_ROOT))
 
-        completed_project = self.client.completed_projects.create(name=self.name)
+    completed_project = self.client.completed_projects.create(name=self.name)
 
-        completed_project.file.name = os.path.join('completed', self.name + '_completed.zip')
-        completed_project.save()
+    completed_project.file.name = os.path.join('completed', self.name + '_completed.zip')
+    completed_project.save()
 
-        zip_file.close()
+    zip_file.close()
 
-        #remove tree
-        sh.rmtree(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed'))
+    #remove tree
+    sh.rmtree(os.path.join(COMPLETED_PROJECT_ROOT, self.name + '_completed'))
 
 class CompletedProject(models.Model):
-    #connections
-    client = models.ForeignKey(Client, related_name='completed_projects')
+  #connections
+  client = models.ForeignKey(Client, related_name='completed_projects')
 
-    #properties
-    name = models.CharField(max_length=255)
-    file = models.FileField(upload_to='completed', null=True, max_length=255)
+  #properties
+  name = models.CharField(max_length=255)
+  file = models.FileField(upload_to='completed', null=True, max_length=255)
 
-    def __unicode__(self):
-        return self.name + '_completed'
+  def __unicode__(self):
+    return self.name + '_completed'
 
 class Job(models.Model):
-    #connections
-    client = models.ForeignKey(Client, related_name='jobs')
-    project = models.ForeignKey(Project, related_name='jobs')
-    user = models.ForeignKey(User, related_name='jobs')
-    #sub: transcriptions (null)
+  #connections
+  client = models.ForeignKey(Client, related_name='jobs')
+  project = models.ForeignKey(Project, related_name='jobs')
+  user = models.ForeignKey(User, related_name='jobs')
+  #sub: transcriptions (null)
 
-    #properties
-    is_active = models.BooleanField(default=True)
-    active_transcriptions = models.IntegerField(editable=False)
-    total_transcription_time = models.DecimalField(max_digits=5, decimal_places=1, default=0.0, editable=False)
-    date_created = models.DateTimeField(auto_now_add=True)
-    time_taken = models.DecimalField(max_digits=5, decimal_places=1, default=0.0, editable=False)
+  #properties
+  is_active = models.BooleanField(default=True)
+  active_transcriptions = models.IntegerField(editable=False)
+  total_transcription_time = models.DecimalField(max_digits=5, decimal_places=1, default=0.0, editable=False)
+  date_created = models.DateTimeField(auto_now_add=True)
+  time_taken = models.DecimalField(max_digits=5, decimal_places=1, default=0.0, editable=False)
 
-    def __unicode__(self):
-        return str(self.project) + ' > #' + str(self.pk) + ': ' + str(self.user)
+  def __unicode__(self):
+    return str(self.project) + ' > #' + str(self.pk) + ': ' + str(self.user)
 
-    #custom methods
-    def get_transcription_set(self):
-        transcriptions = self.project.transcriptions.filter(requests=0)
-        sorted_transcription_set = sorted(transcriptions, key=lambda x: x.utterance, reverse=False)
+  #custom methods
+  def get_transcription_set(self):
+    transcriptions = self.project.transcriptions.filter(requests=0)
+    sorted_transcription_set = sorted(transcriptions, key=lambda x: x.utterance, reverse=False)
 
-        transcription_set = sorted_transcription_set #however many remain
-        if len(sorted_transcription_set) >= self.active_transcriptions:
-            transcription_set = sorted_transcription_set[:self.active_transcriptions] #first 50 transcriptions
+    transcription_set = sorted_transcription_set #however many remain
+    if len(sorted_transcription_set) >= self.active_transcriptions:
+      transcription_set = sorted_transcription_set[:self.active_transcriptions] #first 50 transcriptions
 
-        for transcription in transcription_set:
-            transcription.requests += 1
-            #make date last requested equal to now
-            transcription.date_last_requested = dt.datetime.now()
-            transcription.save()
-            self.transcriptions.add(transcription)
+    for transcription in transcription_set:
+      transcription.requests += 1
+      #make date last requested equal to now
+      transcription.date_last_requested = dt.datetime.now()
+      transcription.save()
+      self.transcriptions.add(transcription)
 
-    def update(self):
-        self.active_transcriptions = self.transcriptions.filter(is_active=True).count()
-        if self.active_transcriptions==0:
-            if self.is_active:
-                self.is_active = False
+  def update(self):
+    self.active_transcriptions = self.transcriptions.filter(is_active=True).count()
+    if self.active_transcriptions==0:
+      if self.is_active:
+        self.is_active = False
 
 class Action(models.Model):
-    #connections
-    job = models.ForeignKey(Job, related_name='actions')
-    user = models.ForeignKey(User, related_name='actions')
+  #connections
+  job = models.ForeignKey(Job, related_name='actions')
+  user = models.ForeignKey(User, related_name='actions')
 
-    #properties
-    button_id = models.CharField(max_length=100)
-    transcription_id = models.CharField(max_length=100)
-    transcription_utterance = models.CharField(max_length=255)
-    date_created = models.DateTimeField(auto_now_add=True)
+  #properties
+  button_id = models.CharField(max_length=100)
+  transcription_id = models.CharField(max_length=100)
+  transcription_utterance = models.CharField(max_length=255)
+  date_created = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
-        return 'job: ' + str(self.job) + ' > "' + self.button_id + '" doing ' + self.transcription_id + ' ' + str(self.date_created)
+  def __unicode__(self):
+      return 'job: ' + str(self.job) + ' > "' + self.button_id + '" doing ' + self.transcription_id + ' ' + str(self.date_created)
 
-    #custom methods
+  #custom methods
 
 class AutocompleteWord(models.Model):
     #connections
