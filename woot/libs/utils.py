635772454e1b56520d8aa81a3cee9260a2632fb4
NicholasPiano <nicholas.d.piano@gmail.com>
2015-01-02 15:41:22 +0000

    speeding up id_token queries

--- woot/libs/utils.py
+++ woot/libs/utils.py
@@ -20,9 +20,8 @@ def generate_id_token(Obj): #expects Obj.objects
   def get_id_token():
     return ''.join([random.choice(chars) for _ in range(8)]) #8 character string
 
-  current_id_tokens = [obj.id_token for obj in Obj.objects.all()]
   id_token = get_id_token()
-  while id_token in current_id_tokens:
+  while Obj.objects.filter(id_token=id_token).count()>0:
     id_token = get_id_token()
 
   return id_token
