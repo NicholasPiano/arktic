792306b664054a1b39ad86c666d02c2ca8a5559b
NicholasPiano <nicholas.d.piano@gmail.com>
2015-01-06 00:37:26 +0000

    Debug false for one sec

--- woot/settings/dev.py
+++ woot/settings/dev.py
@@ -9,7 +9,7 @@ from woot.settings.common import *
 
 ########## DEBUG CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = False
+DEBUG = True
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
 TEMPLATE_DEBUG = DEBUG
