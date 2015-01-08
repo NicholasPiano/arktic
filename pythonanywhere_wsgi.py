570bbeb945d78cef003bffa536c5218dcdf51a8d
NicholasPiano <nicholas.d.piano@gmail.com>
2015-01-08 11:21:28 +0000

    [SOLVED] Database troubles
    I needed to set the password environ variable in the pythonanywhere
    wsgi script. Doh.

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -18,6 +18,7 @@ if path not in sys.path:
     sys.path.append(path)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'woot.settings.prod'
+os.environ['DB_PWD'] = 'uqnhs77f'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
